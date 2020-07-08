8121cad44072c40dc09530c562f5ed23428b2e35
mox692 <55653825+mox692@users.noreply.github.com>
2020-07-08 15:38:41 +0900

    clean code database/seeds/WeeklySeeder.php

--- database/seeds/WeeklySeeder.php
+++ database/seeds/WeeklySeeder.php
@@ -17,41 +17,25 @@ class WeeklySeeder extends Seeder
         $last_monday="";
 
         $ii = 0; 
+        // 2020-2070年分のデータ
         for($ii=2020;$ii<=2070;$ii++){
-
             
             $year   = $ii;
-            $target = array(0, 1);      // 日曜と土曜
-            
+            $target = array(0, 1);   // 日曜と土曜
             $datetime = new DateTime();
             $datetime->setTimezone( new DateTimeZone('Asia/Tokyo') );
-            // 年,月,日
-            $datetime->setDate($year, 1, 1);
-            
-            // 間隔. 1dayごと
-            $interval =  new DateInterval('P1D');
-            
+            $datetime->setDate($year, 1, 1);    // 年,月,日
+            $interval =  new DateInterval('P1D');   // 間隔. 1dayごと
             // 閏年をチェック
             // 閏年だったら366, 平年だったら365
             $days = $datetime->format('L') == '1' ? 366 : 365;
-            
             $result = array();
-            
             for($i=0;$i<$days;$i++){
-                // 土日以外は弾く
                 if( in_array( (int)$datetime->format('w'), $target) ){
                     $result[] = clone $datetime;
-                    // var_dump($result);
                 }
-
                 $datetime->add($interval);
             }
-            
-            // foreach($result as $value){
-            //     echo $value->format("Y/m/d l") . "\n";
-                
-            // }
-
             $count = 0;
             $set_count = 0;
             $datum = [];
@@ -72,42 +56,25 @@ class WeeklySeeder extends Seeder
                             $count ++;
                             var_dump($last_monday);
                         }
-                        
-    
                 }
                 else if($value->format('w') == 1 && $count == 0){
                     $count ++;
                     $monday_data = $value;
                 }
-
                 else if($value->format('w') == 0 && $count == 1){
-                    // $array1 = $value->format("Y/m/d l");
                     $sunday_data = $value->format("Y/m/d");
-                    // array_merge($datum,['monday' => $monday_data, 'sunday' => $sunday_data]);
                     array_push($datum, ['monday' => $monday_data, 'sunday' => $sunday_data, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]);
-                    // $datum = $datum + ['monday' => $monday_data, 'sunday' => $sunday_data];
                     $monday_data = "";
                     $sunday_data = "";
                 }
                 else if($value->format('w') == 1 && $count == 1){
                     $monday_data = $value->format("Y/m/d");
                     $last_monday = $value->format("Y/m/d");
-
-                    // $array1 = $value->format("Y/m/d l");
+           
                 }
             }
-
-            // var_dump($datum);
-
-        
-
             $weekly = new Weekly();
             $weekly->insert($datum);
-
-            // factory(Admin::class, 10)->create(['admin_role_id' => 3])->each(function ($admin) {
-            //     $company = factory(Company::class)->make();
-            //     $admin->company()->save($company);
-            // });
         }
     }
 }
